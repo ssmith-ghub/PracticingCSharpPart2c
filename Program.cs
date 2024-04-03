929b0d31d4b6befd6e38d7c410acbf2f249f0d7c
Stephen Smith <ssmith_138823@outlook.com>
2024-04-02 20:45:11 -0400

    pushing to GitHub

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.WriteLine("Boomer!\tHow are you todayy\b?\nYou are a good boy!");
+            Console.ReadKey();
+
+            // comment
+            /*
+             * 
+             * 
+             */
+            Console.WriteLine("\nI love you buddy!");
+
+        }
+    }
+}
